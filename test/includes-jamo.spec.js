4c779509e5159745789d1db1ffcfef9837ad4fa4
afrontend <autoscripts@gmail.com>
2020-05-07 23:49:40 +0900

    Test using many JA

--- test/includes-jamo.spec.js
+++ test/includes-jamo.spec.js
@@ -3,9 +3,17 @@ const assert = require('assert');
 
 describe('check if the string has a jamo', function () {
   it('hangul string, one JA', () => {
-    assert.equal(includesJamo("가나다", "ㄱ"), true);
-    assert.equal(includesJamo("가나다", "ㄴ"), true);
-    assert.equal(includesJamo("가나다", "ㄷ"), true);
-    assert.equal(includesJamo("가나다", "ㅁ"), false);
+    assert.equal(includesJamo('가나다', ''), false);
+    assert.equal(includesJamo('가나다', '가나다'), false);
+    assert.equal(includesJamo('가나다', 'ㄱ'), true);
+    assert.equal(includesJamo('가나다', 'ㄴ'), true);
+    assert.equal(includesJamo('가나다', 'ㄷ'), true);
+    assert.equal(includesJamo('가나다', 'ㅁ'), false);
+  });
+  it('hangul string, many JA', () => {
+    assert.equal(includesJamo('가나다', ''), false);
+    assert.equal(includesJamo('가나다', 'ㄱㄴㄷ'), true);
+    assert.equal(includesJamo('가나다', 'ㄱㄴㄷㅁ'), false);
+    assert.equal(includesJamo('가나다', 'ㄷㄱㄴ'), true);
   });
 });
